4e4c54b982bd6a71eb92417334402988d91ed579
PDiracDelta <xtrajoris@gmail.com>
2016-09-06 10:49:15 +0200

    main file skeleton

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+'''
+Python implementation of mass spectrometer protein data analysis using the CONSTANd_RAS algorithm
+'''
+
+__author__ = "Joris Van Houtven"
+__copyright__ = "Copyright ?, VITO"
+__credits__ = ["Joris Van Houtven", "Dirk Valkenborg"]
+#__license__ = "GPL"
+#__version__ = "0.0.1"
+__maintainer__ = "Joris Van Houtven"
+__email__ = "vanhoutvenjoris@gmail.com"
+__status__ = "Development"
+
+import sys
+
+
+def main():
+    print("This is the main method.")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
