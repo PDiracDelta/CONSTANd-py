92c9ede54a6fa26adcbe857ac4fb48527b61cd2d
PDiracDelta <xtrajoris@gmail.com>
2016-09-07 17:06:36 +0200

    implemented constand.py but it still has bugs; elaborated the main skeleton (getInput etc)

--- constandpp/main.py
+++ constandpp/main.py
@@ -8,20 +8,45 @@ Python implementation of mass spectrometer protein data analysis using the CONST
 __author__ = "Joris Van Houtven"
 __copyright__ = "Copyright ?, VITO"
 __credits__ = ["Joris Van Houtven", "Dirk Valkenborg"]
-#__license__ = "GPL"
-#__version__ = "0.0.1"
+# __license__ = "GPL"
+# __version__ = "0.0.1"
 __maintainer__ = "Joris Van Houtven"
 __email__ = "vanhoutvenjoris@gmail.com"
 __status__ = "Development"
 
 import sys
-#import pandas as pd
-#import numpy as np
-#import matplotlib as mpl
+import pandas as pd
+import numpy as np
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+import constand as cd
+
+
+def getInput():
+    """Get mass spec data and CONSTANd parameters"""
+    path=None
+    sep='\t'
+    accuracy=1e-2
+    maxIterations=50
+    return path,sep,accuracy,maxIterations
+
+
+def importDataFrame(path=None, sep=','):
+    """Get the data from disk as a Pandas DataFrame"""
+    df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10, 6)), columns=list('ABCDEF'))  # TEST
+    # df = pd.read_csv('../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.txt', sep='\t') # TEST
+    # df = pd.read_csv(path, sep=sep)
+    return df
+
 
 def main():
-    print("This is the main method.")
+    path,sep,accuracy,maxIterations = getInput()
+    df = importDataFrame(path,sep)
+    assert isinstance(df, pd.DataFrame)
+    data = np.asmatrix(df)
+    normalized = cd.constand(data,accuracy,maxIterations)
+    print(data)
+    print(normalized)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
