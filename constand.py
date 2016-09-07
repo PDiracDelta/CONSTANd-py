c7e908721a6ab00bbd2bde8da0ad4d3ec407bfb7
PDiracDelta <xtrajoris@gmail.com>
2016-09-07 17:06:36 +0200

    implemented constand.py but it still has bugs; elaborated the main skeleton (getInput etc)

--- constand.py
+++ constand.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# function [normalizedData,f,R,S] = CONSTANd_RAS(data,h,maxIterations)
 """
 CONSTANd
 Normalizes the data matrix <data> by raking the m by n matrix such that
@@ -18,4 +17,46 @@ Analysis"
 © Dirk Valkenborg & Jef Hooyberghs, 2014
 Ported to Python by Joris Van Houtven, 2016
 """
+
+import numpy as np
+from numpy import nan as NaN
+
+
+def constand(data, accuracy, maxIterations):
+    assert isinstance(data, np.matrix)
+    assert data.dtype is np.dtype('float64')
+    assert accuracy > 0
+    assert maxIterations > 0
+
+    # initialize variables
+    Nrows, Ncols = data.shape
+    convergenceTrail = [NaN]*(2*maxIterations)
+    convergence = np.inf
+    dataT = data.T
+    normalizedData = data
+
+    i = 0 # number of current iteration
+    # without reshaping the ndarrays, they have shape (x,) (no second value) and the procedure fails.
+    Rd = np.ones((Nrows,1)) # R matrix diagonal from RAS
+    Sd = np.ones((Ncols,1)) # S matrix diagonal from RAS
+    # main loop; iterates until convergence is reached (i.e., L1-norm below variable <h>) or the maximum number of
+    # iteration cycles is surpassed.
+    while convergence > accuracy and i < maxIterations:
+
+        # fit the rows
+        Rd = Rd * 1/Ncols * np.asarray(1/np.nanmean(normalizedData,1))
+        normalizedData = (dataT * Rd).T *Sd
+        # calculate deviation from column marginals; row deviation is zero at even indices. (index start = 0)
+        convergenceTrail[2*i] = Nrows * 0.5 * np.nansum(np.abs(np.nanmean(normalizedData,0) - 1/Ncols))
+
+        # fit the columns
+        Sd = Sd * 1/Ncols * np.asarray(1/np.nanmean(normalizedData, 0))
+        normalizedData = (dataT * Rd).T *Sd
+        # calculate deviation from row marginals; column deviation is zero at odd indices. (index start = 0)
+        convergenceTrail[2*i+1] = Ncols * 0.5 * np.nansum(np.abs(np.nanmean(normalizedData, 1) - 1/Ncols))
+
+        convergence = convergenceTrail[2*i]
+        i += 1
+
+    return normalizedData, convergenceTrail, Rd, Sd
 
