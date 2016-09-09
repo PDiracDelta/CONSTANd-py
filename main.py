4cc3dd273e5918a0402ce6fc59ceee8118633fe9
PDiracDelta <xtrajoris@gmail.com>
2016-09-09 09:11:22 +0200

    added performanceTest method so that i dont have to copy paste that code each time.

--- main.py
+++ main.py
@@ -19,38 +19,54 @@ import pandas as pd
 import numpy as np
 # import matplotlib as mpl
 # import matplotlib.pyplot as plt
-import constand as cd
+from constand import constand
+from time import time
 
 
 def getInput():
-    """Get mass spec data and CONSTANd parameters"""
-    path=None
-    sep='\t'
-    accuracy=1e-2
-    maxIterations=50
-    return path,sep,accuracy,maxIterations
+	""" Get mass spec data and CONSTANd parameters """
+	path=None
+	sep='\t'
+	accuracy=1e-2
+	maxIterations=50
+	return path,sep,accuracy,maxIterations
 
 
 def importDataFrame(path=None, sep=','):
-    """Get the data from disk as a Pandas DataFrame"""
-    df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10, 6)), columns=list('ABCDEF'))  # TEST
-    # df = pd.read_csv('../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.txt', sep='\t') # TEST
-    # df = pd.read_csv(path, sep=sep)
-    df = pd.DataFrame(np.arange(10*6).reshape(10,6),columns=list('ABCDEF')) # TEST
-    df['B'][0]=np.nan
-    print(df)
-    return df
+	""" Get the data from disk as a Pandas DataFrame """
+	# df = pd.read_csv(path, sep=sep)
+	# df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10, 6)), columns=list('ABCDEF'))  # TEST
+	# df = pd.read_csv('../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.txt', sep='\t') # TEST
+	# df = pd.DataFrame(np.arange(10*6).reshape(10,6),columns=list('ABCDEF')) # TEST
+	# df['B'][0]=np.nan # TEST
+	df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10**3, 6)), columns=list('ABCDEF'))  # TEST
+	return df
+
+
+def performanceTest():
+	t = []
+	for i in range(1000):
+		df = importDataFrame(path, sep)
+		assert isinstance(df, pd.DataFrame)
+		data = np.asarray(df)  # ndarray instead of matrix because this is more convenient in the calculations
+		start = time()
+		# normalizedData,convergenceTrail,R,S = constand(data,accuracy,maxIterations)
+		constand(data, 1e-2, 50)
+		stop = time()
+		t.append((stop - start))
+	print("average runtime: " + str(np.mean(t)))
 
 
 def main():
-    path,sep,accuracy,maxIterations = getInput()
-    df = importDataFrame(path,sep)
-    assert isinstance(df, pd.DataFrame)
-    data = np.asarray(df) # ndarray instead of matrix because this is more convenient in the calculations
-    normalizedData,convergenceTrail,R,S = cd.constand(data,accuracy,maxIterations)
-    #print(data)
-    print(normalizedData)
-    return normalizedData,convergenceTrail
+	""" For now this is just stuff for debugging and testing """
+	path,sep,accuracy,maxIterations = getInput()
+	df = importDataFrame(path,sep)
+	assert isinstance(df, pd.DataFrame)
+	data = np.asarray(df) # ndarray instead of matrix because this is more convenient in the calculations
+	normalizedData,convergenceTrail,R,S = constand(data,accuracy,maxIterations)
+	
+	performanceTest()
+
 
 if __name__ == '__main__':
-    sys.exit(main())
+	sys.exit(main())
