b2cf3e98dbe9e76650e903ada516b90534904483
PDiracDelta <xtrajoris@gmail.com>
2016-09-13 17:09:32 +0200

    elaborated profiler function (though some more tweaking needed + need to add line_profiler switch, now only has cProfiler)

--- constandpp/profiler.py
+++ constandpp/profiler.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import cProfile
+import cProfile, pstats
 from main import main
 
-
-cProfile.run(main())
+pr = cProfile.Profile()
+pr.enable()
+main()
+pr.disable()
+pr.dump_stats('main.stats')
+outfilename='mainstats.txt'
+with open(outfilename, 'wt') as output:
+    stats = pstats.Stats('main.stats', stream=output)
+    stats.sort_stats('cumulative', 'time')
+    stats.print_stats()
+with open(outfilename, 'r') as fin:
+    print(fin.read())
