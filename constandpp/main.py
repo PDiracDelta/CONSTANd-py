83f220ab05b7026869ef72a1ea1b18688342ca33
PDiracDelta <xtrajoris@gmail.com>
2016-09-15 14:10:13 +0200

    fixed bug in importDataFrame elaborated importDataFrame (added more security checks) elaborated getIntensities (not finished yet)

--- constandpp/main.py
+++ constandpp/main.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-Python implementation of mass spectrometer protein data analysis using the CONSTANd_RAS algorithm
+Python implementation of mass spectrometer protein data analysis using the CONSTANd_RAS algorithm.
 """
 
 __author__ = "Joris Van Houtven"
-__copyright__ = "Copyright ?, VITO"
+__copyright__ = "Copyright ?YEAR, VITO"
 __credits__ = ["Joris Van Houtven", "Dirk Valkenborg"]
 # __license__ = "GPL"
 # __version__ = "0.0.1"
@@ -24,70 +24,89 @@ from time import time
 
 
 def getInput():
-	""" Get mass spec data and CONSTANd parameters """
-	path=None
-	delim='\t'
-	accuracy=1e-2
-	maxIterations=50
-	return path,delim,accuracy,maxIterations
+	""" Get mass spec data and CONSTANd parameters from the user or from the web interface. """
+	# path='../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.tsv' # TEST
+	path = '../data/MB_noapostrophes.tsv'  # TEST
+	delim = '\t'
+	accuracy = 1e-2
+	maxIterations = 50
+	return path, delim, accuracy, maxIterations
 
 
-def importData(path, delim):
+def importData(path=None, delim=None):
+	""" Return the intensity matrix and the dataFrame of the data specified. """
 	# df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10, 6)), columns=list('ABCDEF'))  # TEST
 	# df = pd.read_csv('../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.txt', delim='\t') # TEST
 	# df = pd.DataFrame(np.arange(10*6).reshape(10,6),columns=list('ABCDEF')) # TEST
 	# df['B'][0]=np.nan # TEST
-	df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10**3, 6)), columns=list('ABCDEF'))  # TEST
+	# df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10**3, 6)), columns=list('ABCDEF'))  # TEST
+	df = importDataFrame(path)#, delim=delim)
 	intensities = selectIntensities(df)
-	assert isinstance(intensities, np.ndarray) # ndarray instead of matrix because this is more convenient in the calculations
+	assert isinstance(intensities,
+	                  np.ndarray)  # ndarray instead of matrix because this is more convenient in the calculations
 	return intensities, df
 
 
 def importDataFrame(path=None, filetype=None, delim=None):
-	""" Get the data from disk as a Pandas DataFrame """
+	""" Get the data from disk as a Pandas DataFrame. """
 	assert path is not None
-	if filetype is None: # set filetype equal to the file extension
-		filetype = path.split('.')(-1)
+	if filetype is None:  # set filetype equal to the file extension
+		filetype = path.split('.')[-1]
 
 	if filetype == 'xlsx':
+		raise Exception("this shouldnt be executed")  # TEST
 		df = pd.read_excel(path)
 	elif filetype == 'csv':
-		df = pd.read_csv(path)
+		raise Exception("this shouldnt be executed")  # TEST
+		if delim is None:
+			df = pd.read_csv(path, delimiter=',')
+		else:
+			df = pd.read_csv(path, delimiter=delim)
 	elif filetype == 'tsv':
-		df = pd.read_table(path)
+		if delim is None:
+			df = pd.read_csv(path, delimiter='\t')
+		else:
+			df = pd.read_csv(path, delimiter=delim)
 	else:
-		df = pd.read_table(path, delim=delim)
+		if delim is None:
+			raise Exception(
+				"I don't know how to handle this data: the filetype was not recognized and no delimiter was specified.")
+		raise Warning("Did not recognize filetype: treating as delimited textfile with the delimiter you specified.")
+		df = pd.read_csv(path, delimiter=delim)
 
-	data = np.asarray(df)
-	return data
+	return df
 
 
 def selectIntensities(df):
-	intensities=None
+	""" Extracts the intensity matrix from the dataFrame. """
+	print(df)
+	intensities = None
+	intensities = np.asmatrix(df[['126', '127', '128', '129', '130', '131']])
+	# print(intensities)
 	return intensities
 
 
-def performanceTest(): # remove for production
-	""" Use this development method to test the performance of the CONSTANd algorithm """
+def performanceTest():  # remove for production
+	""" Use this development method to test the performance of the CONSTANd algorithm. """
 	t = []
 	for i in range(1000):
 		path, delim, accuracy, maxIterations = getInput()
-		data = importData(path, delim)
+		intensities, df = importData(path, delim)
 		start = time()
-		constand(data, 1e-2, 50)
+		constand(intensities, 1e-2, 50)
 		stop = time()
 		t.append((stop - start))
 	print("average runtime: " + str(np.mean(t)))
 
 
 def main():
-	""" For now this is just stuff for debugging and testing """
-	path,delim,accuracy,maxIterations = getInput()
-	intensities, df = importData(path,delim)
+	""" For now this is just stuff for debugging and testing. """
+	path, delim, accuracy, maxIterations = getInput()
+	intensities, df = importData(path, delim)
 	assert isinstance(intensities, np.ndarray)
-	normalizedIntensities,convergenceTrail,R,S = constand(intensities,accuracy,maxIterations)
-	#print(normalizedIntensities)
-	#performanceTest()
+	normalizedIntensities, convergenceTrail, R, S = constand(intensities, accuracy, maxIterations)
+	# print(normalizedIntensities)
+	performanceTest()
 
 
 if __name__ == '__main__':
