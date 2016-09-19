e23e31937603e960c7e2bfe0109ee45d2b0d6302
PDiracDelta <xtrajoris@gmail.com>
2016-09-19 13:40:28 +0200

    docu of constand.py completed (for now)

--- constandpp/dataIO.py
+++ constandpp/dataIO.py
@@ -13,12 +13,12 @@ import numpy as np
 def getInput():
 	"""
 	Get mass spec data and CONSTANd parameters from the user or from the web interface.
-	:return path_in:    string  path to the input file
-	:return delim_in:   char    delimiter of the data in the input file
-	:return accuracy:   float   CONSTANd param: allowed deviation of col and row means from 1/6
-	:return maxIterations:  int CONSTANd param: maximum amount of iterations (1x row and 1x col per iteration)
-	:return path_out:   string  path to the output file
-	:return delim_out:  char    delimiter of the data in the output file
+	:return path_in:        string  path to the input file
+	:return delim_in:       char    delimiter of the data in the input file
+	:return accuracy:       float   CONSTANd param: combined allowed deviation of col and row means from 1/6
+	:return maxIterations:  int     CONSTANd param: maximum amount of iterations (1x row and 1x col per iteration)
+	:return path_out:       string  path to the output file
+	:return delim_out:      char    delimiter of the data in the output file
 	"""
 	# path='../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.tsv' # TEST
 	path_in = '../data/MB_noapostrophes.tsv'  # TEST
@@ -33,10 +33,10 @@ def getInput():
 def importData(path=None, delim=None):
 	"""
 	Return the intensity matrix and the dataFrame of the data specified.
-	:param path:    string  path to the data file
-	:param delim:   char    delimiter of the data
-	:return intensities:    np.ndarray  matrix with the absolute intensities
-	:return df: pd.dataframe    Pandas dataframe with the contents of the data file, including the intensities
+	:param path:            string          path to the data file
+	:param delim:           char            delimiter of the data
+	:return intensities:    np.ndarray      (N,6) ndarray with the absolute intensities
+	:return df:             pd.dataframe    Pandas dataframe with the contents of the data file, including the intensities
 	"""
 	# df = pd.DataFrame(np.random.uniform(low=10 ** 3, high=10 ** 5, size=(10, 6)), columns=list('ABCDEF'))  # TEST
 	# df = pd.read_csv('../data/MB_Bon_tmt_TargetPeptideSpectrumMatch.txt', delim='\t') # TEST
@@ -53,7 +53,7 @@ def importData(path=None, delim=None):
 def exportData(data=None, path=None, delim=','):
 	"""
 	Save the results (normalized intensities) to disk.
-	:param data:    obj data object to be exported to disk
+	:param data:    obj     data object to be exported to disk
 	:param path:    string  path+filename where data should be exported to
 	:param delim:   char    delimiter of the data
 	"""
@@ -67,10 +67,10 @@ def exportData(data=None, path=None, delim=','):
 def importDataFrame(path=None, filetype=None, delim=None):
 	"""
 	Get the data from disk as a Pandas DataFrame.
-	:param path:    string  path to input file
-	:param filetype:    string  specifier for the type of the file (file extension)
-	:param delim:   char    delimiter of the data
-	:return df: pd.dataframe    Pandas dataFrame of the file contents
+	:param path:        string          path to input file
+	:param filetype:    string          specifier for the type of the file (file extension)
+	:param delim:       char            delimiter of the data
+	:return df:         pd.dataframe    Pandas dataFrame of the file contents
 	"""
 	assert path is not None
 	if filetype is None:  # set filetype equal to the file extension
@@ -101,8 +101,8 @@ def importDataFrame(path=None, filetype=None, delim=None):
 def selectIntensities(df):
 	"""
 	Extracts the (absolute) intensity matrix from the dataFrame.
-	:param df:  pd.dataframe    Pandas dataFramefrom which to extract the intensities
-	:return intensities:    np.ndarray  matrix with the intensities
+	:param df:              pd.dataframe    Pandas dataFrame from which to extract the intensities
+	:return intensities:    np.ndarray      matrix with the intensities
 	"""
 	intensities = np.asarray(df[['126', '127', '128', '129', '130', '131']])
 	return intensities
