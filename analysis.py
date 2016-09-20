11ae68985d44df1460b08f19a94dea882188ce25
PDiracDelta <xtrajoris@gmail.com>
2016-09-20 14:24:37 +0200

    built the entire workflow skeleton inside the main.py:main() and made barebones files (analysis.py, dataprep.py) with the required functions.

---
+++ analysis.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Performs a differential expression analysis on the normalized intensities as provided by CONSTANd. Includes data visualization.
+"""
+
+
+def differentialExpression(normalizedIntensities):
+	return None
+
+
+def dataVisualization(DEresults):
+	return None
+
+
