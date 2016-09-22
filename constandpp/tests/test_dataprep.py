9cb79b5ff4de1bdde7cf26070e7acf2e11e7f856
PDiracDelta <xtrajoris@gmail.com>
2016-09-22 15:58:56 +0200

    added test_dataprep.py (barebones)

---
+++ constandpp/tests/test_dataprep.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestRemoveIsolationInterference(TestCase):
+	def test_removeIsolationInterference(self):
+		# print(str(df.shape) + ', ' + str(removedData['isolationInterference'].shape)) # shapes must agree / add up
