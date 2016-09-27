220d2f6e12c3bd216505f0af3112bd3391229d30
PDiracDelta <xtrajoris@gmail.com>
2016-09-27 16:50:20 +0200

    fixed a refactoring error

--- constandpp/tests/test_constand.py
+++ constandpp/tests/test_constand.py
@@ -6,17 +6,17 @@ from numpy.testing import assert_array_almost_equal
 
 class TestConstand(TestCase):
 	def setUp(self):
-		self.path_in = '/home/pdiracdelta/Documents/KUL/Master of Bioinformatics/Thesis/data/MB_noapostrophes.tsv'  # TEST
+		self.file_in = '/home/pdiracdelta/Documents/KUL/Master of Bioinformatics/Thesis/data/MB_noapostrophes.tsv'  # TEST
 		self.delim_in = '\t'
 		self.header_in = None
 		self.accuracy = 1e-2
 		self.maxIterations = 50
 		self.path_out = '/home/pdiracdelta/Documents/KUL/Master of Bioinformatics/Thesis/data/MB_result.tsv'  # TEST
 		self.delim_out = '\t'
-		self.intensities, self.df = importData(self.path_in, self.delim_in)
+		self.intensities, self.df = importDataFrame(self.file_in, self.delim_in)
 
 	def test_constand_result(self):
-		correctResult = np.asarray(importDataFrame(path_in=self.path_out, header=self.header_in))
+		correctResult = np.asarray(importDataFrame(file_in=self.path_out, header=self.header_in))
 		normalizedIntensities, convergenceTrail, R, S = constand(self.intensities, self.accuracy, self.maxIterations)
 		assert_array_almost_equal(normalizedIntensities, correctResult,
 		                          err_msg="CONSTANd result inconsistent with earlier calculations.")
