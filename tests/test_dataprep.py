ab98e2351b32f5ec6e7de67efc82afdb896db901
PDiracDelta <xtrajoris@gmail.com>
2016-10-12 12:10:00 +0200

    implementing collapseCharge() but this is a freaking b*tch there are still issues with a) how to detect peaks that ought to be collapsed and b) how to combine the intensities of the duplicates

--- tests/test_dataprep.py
+++ tests/test_dataprep.py
@@ -11,3 +11,15 @@ class TestCollapsePSMAlgo(TestCase):
 	def test_collapsePSMAlgo(self):
 		# test shape of deleted data with known shape from MB_Bon....txt Excel info.
 		self.fail()
+
+
+class TestCollapseCharge(TestCase):
+	def test_collapseCharge(self):
+		# test shape of deleted data with known shape from MB_Bon....txt Excel info.
+		self.fail()
+
+	def test_getDuplicates(self):
+		self.fail()
+
+	def test_updateFirstOccurrences(self):
+		self.fail()
