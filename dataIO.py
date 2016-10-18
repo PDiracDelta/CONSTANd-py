a9e115c9e7a6a3a7baad6033c8426b56f3cbbe6f
PDiracDelta <xtrajoris@gmail.com>
2016-10-18 10:12:34 +0200

    refactor collapseRT_centerMeasure_reporters -> collapseRT_centerMeasure_addition

--- dataIO.py
+++ dataIO.py
@@ -46,8 +46,8 @@ def getInput():
 	collapsePSMAlgo_master = config.get('DEFAULT','collapsePSMAlgo_master')
 	collapsePSMAlgo_exclusive_bool = config.getboolean('DEFAULT','collapsePSMAlgo_exclusive_bool')
 	collapseRT_bool = config.getboolean('DEFAULT','collapseRT_bool')
-	collapseRT_centerMeasure_reporters = config.get('DEFAULT','collapseRT_centerMeasure_reporters')
-	collapseRT_centerMeasure_intensities = config.get('DEFAULT','collapseRT_centerMeasure_intensities')
+	collapseRT_centerMeasure_addition = config.get('DEFAULT','collapseRT_centerMeasure_addition') # how to add peaks?
+	collapseRT_centerMeasure_intensities = config.get('DEFAULT','collapseRT_centerMeasure_intensities') #
 	collapseRT_maxRelativeReporterVariance = config.getfloat('DEFAULT','collapseRT_maxRelativeReporterVariance')
 	collapseCharge_bool = config.getboolean('DEFAULT','collapseCharge_bool')
 	isotopicCorrection_bool = config.getboolean('DEFAULT','isotopicCorrection_bool')
@@ -80,10 +80,10 @@ def getInput():
 		raise Exception("Please indicate whether PSM Algorithm redundancy removal should be exclusive or not.")
 	if collapseRT_bool is None:
 		raise Exception("Please indicate whether you would like to remove redundancy due to multiple retention times.")
-	if collapseRT_centerMeasure_reporters not in ('mean', 'median'):
-		raise Exception("Invalid center measure: '"+collapseRT_centerMeasure_reporters+"'. Please pick 'mean' or 'median'.")
+	if collapseRT_centerMeasure_addition not in ('mean', 'median'):
+		raise Exception("Invalid center measure: '"+collapseRT_centerMeasure_addition+"'. Please pick 'mean' or 'median'.")
 	if collapseRT_centerMeasure_intensities not in ('max', 'mean', 'median'):
-		raise Exception("Invalid center measure: '"+collapseRT_centerMeasure_reporters+"'. "
+		raise Exception("Invalid center measure: '"+collapseRT_centerMeasure_addition+"'. "
 		                                                                              "Please pick 'max', 'mean' or 'median'.")
 	if collapseRT_maxRelativeReporterVariance is not None:
 		if not collapseRT_maxRelativeReporterVariance > 0:
@@ -124,7 +124,7 @@ def getInput():
 		'collapsePSMAlgo_master': collapsePSMAlgo_master,
 		'collapsePSMAlgo_exclusive_bool': collapsePSMAlgo_exclusive_bool,
 		'collapseRT_bool': collapseRT_bool,
-		'collapseRT_centerMeasure_reporters': collapseRT_centerMeasure_reporters,
+		'collapseRT_centerMeasure_addition': collapseRT_centerMeasure_addition,
 		'collapseRT_centerMeasure_intensities': collapseRT_centerMeasure_intensities,
 		'collapseRT_maxRelativeReporterVariance': collapseRT_maxRelativeReporterVariance,
 		'collapseCharge_bool': collapseCharge_bool,
