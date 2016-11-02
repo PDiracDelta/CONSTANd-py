fixed off-by-one error in getRepresentativesDf --> BIG BUGFIX off-by-one + race condition during data manipulation = results non-deterministic

constandpp/main.py
@@ -172,7 +172,7 @@ def main(testing, writeToDisk):
 			                                            undoublePSMAlgo_bool=params['undoublePSMAlgo_bool'])
 
 		# SANITY CHECK: there should be no more duplicates if all collapses have been applied.
-		if params['undoublePSMAlgo_bool'] and params['collapseRT_bool'] and params['collapseCharge_bool']:
+		if params['undoublePSMAlgo_bool'] and params['collapseRT_bool'] and params['collapseCharge_bool']: # TEST
 			assert np.prod((len(i) < 2 for (s, i) in df.groupby('Annotated Sequence').groups)) # only 1 index vector in dict of SEQUENCE:[INDICES] for all sequences
 
 		if params['isotopicCorrection_bool']: