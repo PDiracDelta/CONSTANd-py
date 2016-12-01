6d4c764611057e04873160c2b47d25468a52741b
PDiracDelta <xtrajoris@gmail.com>
2016-12-01 19:19:23 +0100

    PCA plot now colours channels according to their corresponding condition PCA plot now has distinguishable colors for arbitrary number of conditions.

--- report.py
+++ report.py
@@ -11,6 +11,7 @@ Functions involved in generating the report that includes:
 * overview of parameters used in the workflow
 """
 import pandas as pd
+import numpy as np
 from warnings import warn
 from scipy.cluster.hierarchy import dendrogram
 from matplotlib import pyplot as plt
@@ -18,7 +19,7 @@ from matplotlib import pyplot as plt
 # save matplotlib images without whitespace: savefig('foo.png', bbox_inches='tight')
 
 
-def dataVisualization(minProteinDF, fullProteinDF, alpha, FCThreshold, PCAResult, HCResult):
+def dataVisualization(minProteinDF, fullProteinDF, alpha, FCThreshold, PCAResult, HCResult, intensityColumnsPerCondition):
 	# TODO (if paying customer): parameter: intensity matrix on peptide or protein level?
 	# TODO: only include differentials with a fold of >threshold or <1/threshold
 	visualizationsDict = {}
@@ -31,9 +32,18 @@ def dataVisualization(minProteinDF, fullProteinDF, alpha, FCThreshold, PCAResult
 	plt.title('Principal Component scores', figure=PCAPlot)
 	plt.xlabel('First PC', figure=PCAPlot)
 	plt.ylabel('Second PC', figure=PCAPlot)
-	plt.scatter(PCAResult[:, 0], PCAResult[:, 1], c=['b', 'r'], figure=PCAPlot) # plot first two principal components
+	# get distinguishable colours
+	hsv = plt.get_cmap('hsv')
+	nConditions = len(intensityColumnsPerCondition) # number of TMT channels
+	distinguishableColors = hsv(np.linspace(0, 1.0, nConditions))
+	# generate colors vector so that the channels of the same condition have the same colour
+	colors = []
+	for condition in range(nConditions):
+		colors.append([distinguishableColors[condition],]*len(intensityColumnsPerCondition[condition]))
+	# produce scatterplot
+	plt.scatter(PCAResult[:, 0], PCAResult[:, 1], c=colors, figure=PCAPlot) # plot first two principal components
 	visualizationsDict['pca'] = PCAPlot
-	plt.show()  # TEST
+	plt.show() # TEST
 
 	# hierarchical clustering dendrogram
 	HCDendrogram = plt.figure(figsize=(6, 5)) # size(inches wide, height); a4paper: width = 8.267in; height 11.692in
