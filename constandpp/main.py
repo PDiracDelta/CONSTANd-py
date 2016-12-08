7948d874c38605d1506733300ac92a892ec7a315
PDiracDelta <xtrajoris@gmail.com>
2016-12-08 17:22:28 +0100

    refactoring: getAllExperimentsIntensitiesPerCommonPeptide

--- constandpp/main.py
+++ constandpp/main.py
@@ -286,11 +286,11 @@ def analyzeProcessingResult(processingResults, params, writeToDisk):
 	fullProteinDF = applySignificance(fullProteinDF, params['alpha'], params['FCThreshold'])
 
 	# dataframe with ALL intensities per peptide: [peptide, e1_channel1, e1_channel2, ..., eM_channel1, ..., eM_channelN]
-	allExperimentsIntensitiesPerPeptide = getAllExperimentIntensitiesPerPeptide(dfs, params['schema'])
+	allExperimentsIntensitiesPerCommonPeptide = getAllExperimentsIntensitiesPerCommonPeptide(dfs, params['schema'])
 	# perform PCA
-	PCAResult = getPCA(allExperimentsIntensitiesPerPeptide, params['PCA_components'])
+	PCAResult = getPCA(allExperimentsIntensitiesPerCommonPeptide, params['PCA_components'])
 	# perform hierarchical clustering
-	HCResult = getHC(allExperimentsIntensitiesPerPeptide)
+	HCResult = getHC(allExperimentsIntensitiesPerCommonPeptide)
 
 	# set the protein names back as columns instead of the index, and sort the columns so the df is easier to read
 	handyColumnOrder = ['protein', 'significant', 'adjusted p-value', 'log2 fold change c1/c2', 'description', 'p-value', 'peptides', 'condition 1', 'condition 2']
