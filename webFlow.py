f808035577ad8f29f9a6b8fed9853cee93eaf309
PDiracDelta <xtrajoris@gmail.com>
2016-12-10 21:46:47 +0100

    bugfix: forgot to dumps channelAliasesPerCondition

--- webFlow.py
+++ webFlow.py
@@ -79,7 +79,7 @@ def webFlow():
 				fout.write('data = ' + experiment['data'] + '\n')
 				fout.write('wrapper = ' + experiment['wrapper'] + '\n')
 					# caution! use the ALIASES, and NOT the original names (they are rewritten by the wrapper)
-				fout.write('intensityColumnsPerCondition = ' + experiment['channelAliasesPerCondition'] + '\n')
+				fout.write('intensityColumnsPerCondition = ' + dumps(experiment['channelAliasesPerCondition']) + '\n')
 				fout.write('isotopicCorrection_matrix = ' + experiment['isotopicCorrection_matrix'] + '\n')
 				# write output parameters
 				fout.write('path_out = '+str(os.path.join(this_job_path, 'output_processing/'))+'\n')
