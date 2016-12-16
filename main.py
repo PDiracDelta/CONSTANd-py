789ff9619f61e510e0f070527b11eac9570c16bb
PDiracDelta <xtrajoris@gmail.com>
2016-12-16 16:05:10 +0100

    bugfix if ICMFile would be None

--- main.py
+++ main.py
@@ -338,7 +338,7 @@ if __name__ == '__main__':
 	#masterConfigFilePath = webFlow(exptype='COON_SN_norm')  # todo constand uitzetten
 	#masterConfigFilePath = webFlow(exptype='COON_SN_norm', previousjobdirName='2016-12-12 22:48:30.701250_COON_SN_norm')  # todo constand uitzetten
 	#masterConfigFilePath = webFlow(exptype='COON_nonormnoconstand')  # todo constand uitzetten
-	masterConfigFilePath = webFlow(exptype='COON')
+	masterConfigFilePath = webFlow(exptype='COON_noISO')
 
 	sys.exit(main(jobConfigFilePath=masterConfigFilePath, doProcessing=True, doAnalysis=True, doReport=True,
 	              testing=False, writeToDisk=True))
