ea98a14a891fcc5bab63eb13ea811b26ee2861c7
PDiracDelta <xtrajoris@gmail.com>
2017-01-16 17:06:05 +0100

    implemented mailer (untested) implementing css

--- constandpp/web/views.py
+++ constandpp/web/views.py
@@ -1,6 +1,12 @@
-from web import app
+import os
+from web import app, mailer
 from flask import render_template, send_from_directory
+from flask_mail import Message
 
+
+#############################
+#Client side
+#############################
 
 @app.route('/')
 def hello_world():
@@ -15,6 +21,25 @@ def hello_world():
 def report(file=None):
 	return render_template(file)
 
+
 @app.route('/jobs/<path:filename>')
-def getImage(filename):
+def getFile(filename):
 	return send_from_directory('../../doc/figures/reportexample/', filename, as_attachment=True)
+
+
+#############################
+#Admin functions
+#############################
+
+def send_mail(recipient, mailBodyFile, jobname, jobID, attachment): # TODO VITO credentials
+	subject = "Inschijving ULYSSIS workshop: %s" % (event_title)
+	#body = "=== English version below ===\n\n"
+	#body += "Beste {0} {1},\n\n"
+	with open(os.path.join('static', mailBodyFile), 'r') as f:
+		body = f.read()
+	body = body.format(recipient, jobname, jobID)
+
+	msg = Message(subject, recipients=[recipient], body=body, sender=("ULYSSIS", "ulyssis@ulyssis.org"))
+	assert os.path.exists(attachment)
+	msg.attach(attachment)
+	mailer.send(msg)
