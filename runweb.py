9740673610be55234663767433397c6838c561ef
PDiracDelta <xtrajoris@gmail.com>
2017-01-16 15:31:46 +0100

    implemented getFile for rendering images (or files) from different locations

--- runweb.py
+++ runweb.py
@@ -4,14 +4,7 @@ __author__ = 'Joris Van Houtven'
 __maintainer__ = "Joris Van Houtven"
 __email__ = "vanhoutvenjoris@gmail.com"
 
-from flask import Flask
-from flask_mail import Mail
 
-app = Flask(__name__)
-#app.config.from_object('config')
-#db = SQLAlchemy(app)
-mail = Mail(app)
-
-from web import views
+from web import app
 
 app.run(debug=False, host='0.0.0.0')
---
+++ web/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_mail import Mail
+
+app = Flask(__name__)
+#app.config.from_object('config')
+#db = SQLAlchemy(app)
+mail = Mail(app)
+
+from web import views
