87f1029867cdf1ba07dd18ccb9cd419cb51f26d6
PDiracDelta <xtrajoris@gmail.com>
2017-01-16 14:27:38 +0100

    created link to dummy report file

--- web/views.py
+++ web/views.py
@@ -1,9 +1,16 @@
 from runweb import app
+from flask import render_template
+
 
 @app.route('/')
 def hello_world():
-	from main import main
-	from web.webFlow import webFlow
-	masterConfigFilePath = webFlow(exptype='COON')
-	main(jobConfigFilePath=masterConfigFilePath, doProcessing=True, doAnalysis=True, doReport=True, testing=False, writeToDisk=True)
-	return 'Hello, Crazy World!'
+	#from main import main
+	#from web.webFlow import webFlow
+	#masterConfigFilePath = webFlow(exptype='COON')
+	#main(jobConfigFilePath=masterConfigFilePath, doProcessing=True, doAnalysis=True, doReport=True, testing=False, writeToDisk=True)
+	return render_template('index.html', reportFile='reportexample.html')
+
+
+@app.route('/report/<file>')
+def report(file=None):
+	return render_template(file)
