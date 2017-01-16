4f80d6d4ff9b0cf4cd8a50edd04c85936815b1a6
PDiracDelta <xtrajoris@gmail.com>
2017-01-16 19:07:03 +0100

    made schemaForm()

--- constandpp/web/forms.py
+++ constandpp/web/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, RadioField, BooleanField, IntegerField, DateTimeField, SelectField, FileField
+from wtforms.validators import DataRequired
+#from werkzeug.utils import secure_filename
 
 
-class SchemaForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+class schemaForm(FlaskForm):
+    schema = FileField('schema', validators=[DataRequired()])
