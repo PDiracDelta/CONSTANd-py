cbd0afcb92f1a54181ea8a822314592932f6fb17
PDiracDelta <xtrajoris@gmail.com>
2017-01-17 13:38:07 +0100

    added config file

--- constandpp/web/__init__.py
+++ constandpp/web/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 
 app = Flask(__name__)
-#app.config.from_object('config')
+app.config.from_object('config')
 #db = SQLAlchemy(app)
 mailer = Mail(app)
 
