4f7e50a01ebfbad6ea801741dfc6b553796351db
PDiracDelta <xtrajoris@gmail.com>
2017-01-17 14:28:33 +0100

    fixed schema upload added newjob_content.html to be included in newjob.html and index.html

--- web/forms.py
+++ web/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 #from flask_wtf.file import FileField
 from wtforms import StringField, RadioField, BooleanField, IntegerField, SelectField, FileField
 from wtforms.validators import DataRequired
 #from werkzeug.utils import secure_filename
 
 
-class newJobForm(Form):
+class newJobForm(FlaskForm):
 	jobName = StringField('job name', validators=[DataRequired()])
 	schema = FileField('schema', validators=[DataRequired()])
--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 import os
 from web import app, mailer
-from flask import render_template, send_from_directory, request, redirect, url_for
+from flask import render_template, send_from_directory, request, redirect, url_for, session
 from flask_mail import Message
 from .forms import newJobForm
 from werkzeug.utils import secure_filename
@@ -16,7 +16,7 @@ def home():
 	#from web.webFlow import webFlow
 	#masterConfigFilePath = webFlow(exptype='COON')
 	#main(jobConfigFilePath=masterConfigFilePath, doProcessing=True, doAnalysis=True, doReport=True, testing=False, writeToDisk=True)
-	return render_template('index.html', reportFile='reportexample.html', form=newJobForm(csrf_enabled=False))
+	return render_template('index.html', reportFile='reportexample.html', form=newJobForm())#csrf_enabled=False))
 
 
 @app.route('/report/<file>')
@@ -36,29 +36,31 @@ def documentation():
 
 @app.route('/newjob', methods=['GET', 'POST'])
 def newjob():
-	form = newJobForm(request.form, csrf_enabled=False)
+	#form = newJobForm(request.form)#, csrf_enabled=False)
+	form = newJobForm()
 	#if request.method == 'POST' and form.validate():
 	if form.validate_on_submit():
 		jobName = form.jobName.data
 		from web.web import newJobDir
 		jobDir = newJobDir(jobName)
 		#schemaFileName = 'schema_'+secure_filename(request.files[form.schema.name])#form.schema.data.filename)
-		schemaFileName = 'schema_' + secure_filename(request.files[request.files['schema']])
+		schemaFileName = 'schema_' + secure_filename(form.schema.data.filename)
 		schemaFilePath = os.path.join(jobDir, schemaFileName)
-		form.schema.file.save(schemaFilePath)
+		form.schema.data.save(schemaFilePath)
 		from dataIO import parseSchemaFile
 		try:
-			incompleteSchema = parseSchemaFile(schemaFilePath)
+			session['incompleteSchema'] = parseSchemaFile(schemaFilePath)
 		except Exception as e:  # remove file and job dir if something went wrong
 			os.remove(schemaFilePath)
 			os.removedirs(jobDir)
 			return redirect(url_for('newjob')) # todo give error message
-		return redirect(url_for('jobSettings', incompleteSchema))
+		return redirect(url_for('jobSettings'))
 	return render_template('newjob.html', form=form)
 
 
 @app.route('/jobSettings', methods=['GET', 'POST'])
-def jobSettings(incompleteSchema):
+def jobSettings():
+	incompleteSchema = session.get('incompleteSchema')
 	return str(incompleteSchema)
 
 
