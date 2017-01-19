d931f0e12ee71374d25b25a9d12452d92d3dfd15
PDiracDelta <xtrajoris@gmail.com>
2017-01-19 16:40:11 +0100

    filled jobSettingsForm with more options

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 #from flask_wtf.file import FileField
-from wtforms import StringField, RadioField, BooleanField, IntegerField, SelectField, FileField, FieldList, FormField
+from wtforms import StringField, RadioField, BooleanField, IntegerField, SelectField, FileField, FieldList, FormField, FloatField
 from wtforms.validators import DataRequired
 #from werkzeug.utils import secure_filename
 
@@ -18,3 +18,10 @@ class experimentForm(FlaskForm):
 
 class jobSettingsForm(FlaskForm):
 	experiments = FieldList(FormField(experimentForm))
+	pept2protCombinationMethod = SelectField('pept2protCombinationMethod', choices=[('mean', 'mean'), ('median', 'median')], default=('mean', 'mean'))
+	alpha = FloatField(default=0.05)
+	FCThreshold = FloatField(default=1)
+	labelVolcanoPlotAreas = StringField(default='[true, false, false, false]')
+	minExpression_bool = BooleanField(default=True)
+	fullExpression_bool = BooleanField(default=False)
+	numDifferentials = IntegerField(default=10)
