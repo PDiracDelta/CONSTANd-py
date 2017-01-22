70256303eb5d3d579f62c533f24279cfb32032d9
PDiracDelta <xtrajoris@gmail.com>
2017-01-22 22:22:12 +0100

    HTML and PDF reports now show via jobInfo()

--- web/config.py
+++ web/config.py
@@ -6,6 +6,7 @@ SECRET_KEY = 'azezfhbvdujhjnzeosdkohefsduiqs'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 UPLOAD_FOLDER = '/home/pdiracdelta' # filesystem path where uploaded posters will be stored
+ALLJOBSDIR = '/home/pdiracdelta/Documents/KUL/Master of Bioinformatics/Thesis/jobs/'
 ALLOWED_EXTENSIONS = {'tsv', 'txt', 'xlsx', 'csv', 'xls', ''}  # Allowed extensions for uploads
 BASEPROCESSINGCONFIG = os.path.abspath(os.path.join(basedir,'../baseProcessingConfig.ini'))
 MAIN = '/home/pdiracdelta/Documents/KUL/Master of Bioinformatics/Thesis/scripts/main.py'
