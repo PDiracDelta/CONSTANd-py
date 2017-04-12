fc6db668e43fa149db8fce274347ae9163c43556
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-04-12 16:00:14 +0200

    docu getColours

--- report.py
+++ report.py
@@ -44,6 +44,7 @@ def getColours(schema, allChannelAliases):
 	Returns list of colours for all the channels in all experiments (based on schema) so that the channels of the same
 	condition have the same colour.
 	:param schema:  			dict    schema of the experiments' hierarchy' hierarchy
+	:param allChannelAliases:	list	unnested channelAliasesPercondition of all experiments, concatenated in order.
 	:return channelColoursDict:	dict    colour for each channel; a different one for each condition
 	"""
 	numConditions = len(list(schema.values())[0]['channelAliasesPerCondition'])
