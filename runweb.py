908f185cc9565e9b352eac3b6b8c4d8b7d342f86
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-04-13 09:56:42 +0200

    docu runweb.py

--- runweb.py
+++ runweb.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+"""
+This file merely creates and runs the Flask web app object.
+"""
+
 __author__ = 'Joris Van Houtven'
 __maintainer__ = "Joris Van Houtven"
 __email__ = "vanhoutvenjoris@gmail.com"
@@ -7,4 +12,5 @@ __email__ = "vanhoutvenjoris@gmail.com"
 
 from web import app
 
+# host=0.0.0.0 makes the server publicly available.
 app.run(debug=True, host='0.0.0.0')
