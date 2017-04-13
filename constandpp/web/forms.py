27fc597e4d12f997d9f1a22738ca936e51d1389f
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-04-13 15:20:55 +0200

    docu forms.py

--- constandpp/web/forms.py
+++ constandpp/web/forms.py
@@ -1,22 +1,33 @@
+"""
+Definition of all the wtforms used in the web app.
+"""
+
 from flask_wtf import FlaskForm
-#from flask_wtf.file import FileField
 from wtforms import StringField, BooleanField, IntegerField, SelectField, FileField, FieldList, FormField, FloatField
 from wtforms.validators import DataRequired
-#from werkzeug.utils import secure_filename
 
 
 class newJobForm(FlaskForm):
+	"""
+	1: create a new job and upload a schema.
+	"""
 	jobName = StringField('job name', validators=[DataRequired()])
 	schema = FileField('schema', validators=[DataRequired()])
 
 
 class experimentForm(FlaskForm):
+	"""
+	2.1: upload the experiment files, config files and wrappers.
+	"""
 	dataFile = FileField('dataFile', validators=[DataRequired()])
 	processingConfig = FileField('processingConfig')
 	wrapper = FileField('wrapper')
 
 
 class jobSettingsForm(FlaskForm):
+	"""
+	2: upload input files (see 2.1) and set the parameters for the job.
+	"""
 	experiments = FieldList(FormField(experimentForm))
 	pept2protCombinationMethod = SelectField('pept2protCombinationMethod', choices=[('mean', 'mean'), ('median', 'median')], default=('mean', 'mean'))
 	alpha = FloatField(default=0.05)
