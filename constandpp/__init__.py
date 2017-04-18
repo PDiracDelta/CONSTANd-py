c0450e381813cbb2adeaae9067f70143e5c179c6
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-04-18 11:11:51 +0200

    added __init__.py and moved credentials there

---
+++ constandpp/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Python implementation of mass spectrometer protein data analysis using the CONSTANd_RAS algorithm.
+"""
+
+__author__ = "Joris Van Houtven"
+__copyright__ = "Copyright ?YEAR, VITO"
+__credits__ = ["Joris Van Houtven", "Dirk Valkenborg"]
+# __license__ = "GPL"
+# __version__ = "0.1"
+__maintainer__ = "Joris Van Houtven"
+__email__ = "vanhoutvenjoris@gmail.com"
+__status__ = "Development"
