73be83973dd7d852ccdcba8c7ee5f5be792e785c
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-04-21 18:02:03 +0200

    filled setup.py correctly
    fixed imports due to move

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='funniest',
+setup(name='constandpp',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
+      description='CONSTANd++ workflow',
+      url='https://constand.vito.be',
+      author='Joris Van Houtven',
+      author_email='joris.vanhoutven@uhasselt.be',
+      license='VITO',
+      packages=find_packages(),
       zip_safe=False)
