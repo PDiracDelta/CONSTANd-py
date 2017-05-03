03359c47955882c6c05d51159bb247aa095fb16a
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-05-03 10:58:16 +0200

    web.py: updated TMT2ICM() to use the rescaling method in the case that TMT IDT values are percentages w.r.t. the main isotope value (which is always 100).

--- constandpp/dataIO.py
+++ constandpp/dataIO.py
@@ -24,26 +24,26 @@ def importDataFrame(path_in, delim=None, header=0, dtype=None):
 	:return df:         pd.dataFrame    Pandas dataFrame of the file contents
 	"""
 	assert os.path.exists(path_in)
-
+	
 	if delim is None:
-		if '.' in path_in: # file has an extension
-			extension = path_in.split('.')[-1] # set extension equal to the file extension (can return None)
+		if '.' in path_in:  # file has an extension
+			extension = path_in.split('.')[-1]  # set extension equal to the file extension (can return None)
 			delim = ext2delim(extension)
 		else:
 			warn("No file extension nor delimiter specified; Pandas will try to automatically detect the delimiter.")
-
+	
 	if delim == 'xlsx':
 		df = pd.read_excel(path_in)
-	else: # delim is something else OR None.
+	else:  # delim is something else OR None.
 		try:
 			df = pd.read_csv(path_in, delimiter=delim, header=header, dtype=dtype)
 		except:
 			if delim is None:
 				raise Exception("Data cannot be read: no delimiter specified and Pandas failed automatic recognition.")
 			else:
-				raise Exception("Data cannot be read: the delimiter "+str(delim)+" is not right for this file.")
-
-	return df.dropna(how="all") # drop empty lines
+				raise Exception("Data cannot be read: the delimiter " + str(delim) + " is not right for this file.")
+	
+	return df.dropna(how="all")  # drop empty lines
 
 
 def importIsotopicCorrectionsMatrix(path_in):
@@ -52,7 +52,7 @@ def importIsotopicCorrectionsMatrix(path_in):
 	:param path_in: str         path of the isotopic corrections matrix file
 	:return :    	np.ndarray  isotopic corrections matrix
 	"""
-	return np.asmatrix(importDataFrame(path_in, delim='\t', header=None)).astype('float64') # make sure its float64
+	return np.asmatrix(importDataFrame(path_in, delim='\t', header=None)).astype('float64')  # make sure its float64
 
 
 def importTMTIsotopicDistributions(path_in):
@@ -72,7 +72,7 @@ def importTMTIsotopicDistributions(path_in):
 		tmtid.set_index('IDT', drop=True, inplace=True)
 	else:
 		raise Exception("Column header of the channels in the TMT isotopic distributions .tsv file should be 'IDT'.")
-	if set(tmtid.columns.values) != set(['-2','-1','+1','+2']):
+	if set(tmtid.columns.values) != set(['-2', '-1', '+1', '+2']):
 		raise Exception("TMT isotopic distributions .tsv file should contain columns '[-2, -1, +1, +2]'.")
 	tmtid.index = tmtid.index.values.astype(str)
 	return tmtid
@@ -102,25 +102,25 @@ def parseSchemaFile(schemaPath):  # todo either move this to web.py or redistrib
 	from collections import OrderedDict
 	# import schema file as dataframe and replace nan values by empty strings
 	schemaDF = importDataFrame(schemaPath, delim='\t', header=None, dtype=str).replace(np.nan, '', regex=True)
-	incompleteSchemaDict = OrderedDict() # use ordered dict so the items() order is always the same
-	assert np.mod(len(schemaDF), 2) == 0 # schema must have even number of lines
-	assert len(schemaDF.columns) > 2 # at least 3 columns
-	for i in range(int(len(schemaDF)/2)):
-		thisRow = schemaDF.loc[2*i, :]
+	incompleteSchemaDict = OrderedDict()  # use ordered dict so the items() order is always the same
+	assert np.mod(len(schemaDF), 2) == 0  # schema must have even number of lines
+	assert len(schemaDF.columns) > 2  # at least 3 columns
+	for i in range(int(len(schemaDF) / 2)):
+		thisRow = schemaDF.loc[2 * i, :]
 		experimentName = str(thisRow[0])
-		numConditions = len(thisRow)-1
-		channelNamesPerCondition = [str(thisRow[c]).split(',') for c in range(1, numConditions+1)]
-		nextRow = schemaDF.loc[2*i+1, :]
+		numConditions = len(thisRow) - 1
+		channelNamesPerCondition = [str(thisRow[c]).split(',') for c in range(1, numConditions + 1)]
+		nextRow = schemaDF.loc[2 * i + 1, :]
 		# if the next row contains just as many values (that are not empty); aliases are provided
 		if len(nextRow) == len(thisRow) and np.sum(nextRow == '') == 0:
-			channelAliasesPerCondition = [str(nextRow[c]).split(',') for c in range(1, numConditions+1)]
-		else: # aliases not (properly) provided
+			channelAliasesPerCondition = [str(nextRow[c]).split(',') for c in range(1, numConditions + 1)]
+		else:  # aliases not (properly) provided
 			shortExperimentName = str(nextRow[0])
 			channelAliasesPerCondition = [[shortExperimentName + '_' + channelName for channelName in condition]
 										  for condition in channelNamesPerCondition]
-
+		
 		incompleteSchemaDict[experimentName] = {'channelNamesPerCondition': channelNamesPerCondition,
-									  'channelAliasesPerCondition': channelAliasesPerCondition}
+												'channelAliasesPerCondition': channelAliasesPerCondition}
 	return incompleteSchemaDict
 
 
@@ -148,14 +148,14 @@ def fixFixableFormatMistakes(df):
 	:return df: pd.DataFrame    data without recognized format mistakes.
 	"""
 	# you've enabled "show flanking amino acids": DIRk --> [L].DIRk.[m]
-	if df.sample(n=1)['Annotated Sequence'].item().count('.') == 2: # sequence contains 2 dots
-		df['Annotated Sequence'] = df['Annotated Sequence'].apply(lambda x: x.split('.')[1]) # select part between dots
-
+	if df.sample(n=1)['Annotated Sequence'].item().count('.') == 2:  # sequence contains 2 dots
+		df['Annotated Sequence'] = df['Annotated Sequence'].apply(lambda x: x.split('.')[1])  # select part between dots
+	
 	# you're using "Identifying Node" instead of "Identifying Node Type"
 	if 'Identifying Node' in df.columns.values and 'Identifying Node Type' not in df.columns.values:
 		# strip the everything after the last space (=node number between parentheses) + the last space.
 		df['Identifying Node Type'] = df['Identifying Node'].apply(lambda s: s.rsplit(' ', 1)[0])
-
+	
 	return df
 
 
@@ -199,36 +199,37 @@ def exportData(data, dataType, path_out, filename, delim_out=None, inOneFile=Fal
 	"""
 	# todo this function should call sub-functions per data type and optional arguments
 	assert os.path.exists(path_out)
-
+	
 	extension = delim2ext(delim_out)
 	fullPath = path_out + '/' + filename + extension
-
+	
 	if dataType == 'txt':
 		np.savetxt(fullPath, data, delimiter=delim_out)
 	elif dataType == 'obj':
 		pickle.dump(data, open(fullPath, 'wb'))
 	elif dataType == 'df':
-		if isinstance(data, dict): # there are actually multiple dataFrames
-			if inOneFile: # save all removedData in one file.
-				removedData=pd.DataFrame().append(list(data.values()))
+		if isinstance(data, dict):  # there are actually multiple dataFrames
+			if inOneFile:  # save all removedData in one file.
+				removedData = pd.DataFrame().append(list(data.values()))
 				# data['missing'].columns contains all possible columns
-				removedData.to_csv(path_out + '/' + filename + extension, sep=delim_out, index=False, columns=data['missing'].columns)
-			else: # save all removedData in separate files per category.
+				removedData.to_csv(path_out + '/' + filename + extension, sep=delim_out, index=False,
+								   columns=data['missing'].columns)
+			else:  # save all removedData in separate files per category.
 				for frameName, frame in data.items():
 					assert isinstance(frame, pd.DataFrame)
-					frame.to_csv(path_out + '/' + filename + '_' + frameName + extension, sep=delim_out,index=False)
+					frame.to_csv(path_out + '/' + filename + '_' + frameName + extension, sep=delim_out, index=False)
 		else:
 			assert isinstance(data, pd.DataFrame)
 			data.to_csv(fullPath, sep=delim_out, index=False)
 	elif dataType == 'fig':
 		outFileFullPathNoExt = os.path.join(path_out, filename)
-		with open(outFileFullPathNoExt+'.pkl', "wb") as fout:
+		with open(outFileFullPathNoExt + '.pkl', "wb") as fout:
 			# save as pickle. Load again later as: ax = pickle.load(file('myplot.pickle')); then plt.show()
 			pickle.dump(data, fout, protocol=4)
 		from matplotlib import pyplot as plt
-		#ax = data
-		plt.savefig(outFileFullPathNoExt+'.png', format='png', bbox_inches='tight')
-		return outFileFullPathNoExt+'.png'
+		# ax = data
+		plt.savefig(outFileFullPathNoExt + '.png', format='png', bbox_inches='tight')
+		return outFileFullPathNoExt + '.png'
 	elif dataType == 'html':
 		fullPath += '.html'
 		with open(fullPath, 'w') as htmlFile:
@@ -240,9 +241,9 @@ def delim2ext(delim):
 	"""
 	Returns the filename extension belonging to the specified delimiter, or returns empty string.
 	"""
-	if delim==',':
+	if delim == ',':
 		return '.csv'
-	elif delim=='\t':
+	elif delim == '\t':
 		return '.tsv'
 	else:
 		return ''
@@ -253,9 +254,9 @@ def ext2delim(ext):
 	Returns the delimiter belonging to the specified filename extension, or returns empty string.
 	"""
 	ext.lstrip('.')
-	if ext=='csv':
+	if ext == 'csv':
 		return ','
-	elif ext=='tsv':
+	elif ext == 'tsv':
 		return '\t'
 	else:
 		return None
