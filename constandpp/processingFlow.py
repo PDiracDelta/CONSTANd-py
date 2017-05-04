1fc053b18468d6706552e7f38549d9d59fc3a472
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-05-04 15:20:32 +0200

    moved isotopicCorrection() step to before the collapse/aggregation steps, because aggregation might already fiddle with the channel ratios. There is now no more i-TRAQ copyright issue since it has become impossible for an end user to extract the naked effect of the isotopic correction method. That is actually because CONSTANd is always applied for end-users

--- constandpp/processingFlow.py
+++ constandpp/processingFlow.py
@@ -29,16 +29,19 @@ def processDf(df, params, writeToDisk, doConstand=True):
 	removedData = {}  # is to contain basic info about data that will be removed during the workflow, per removal category.
 	# remove detections where (essential) data is missing.
 	df, removedData['missing'] = removeMissing(df, params['noMissingValuesColumns'], params['intensityColumns'])
+	
 	if params['removeBadConfidence_bool']:
 		df, removedData['confidence'] = removeBadConfidence(df, params['removeBadConfidence_minimum'], params['removalColumnsToSave'])
 	# remove all useless columns from the dataFrame
 	df = removeObsoleteColumns(df, wantedColumns=params['wantedColumns'])
+	
 	if params['removeIsolationInterference_bool']:
 		# remove all data with too high isolation interference
 		df, removedData['isolationInterference'] = removeIsolationInterference(df, params[
 			'removeIsolationInterference_threshold'], params['removalColumnsToSave'])
 	# remove all non-master protein accessions (entire column) and descriptions (selective).
 	df = setMasterProteinDescriptions(df)
+	
 	if params['undoublePSMAlgo_bool']:
 		# collapse peptide list redundancy due to overlap in MASCOT/SEQUEST peptide matches
 		df, removedData['PSMAlgo'] = undoublePSMAlgo(df, identifyingNodes=params['identifyingNodes'],
@@ -47,16 +50,26 @@ def processDf(df, params, writeToDisk, doConstand=True):
 													 removalColumnsToSave=params['removalColumnsToSave'])
 		# SANITY CHECK: no detections with the same scan number may exist after undoublePSMAlgo()
 		assert np.prod((len(i) < 2 for (s, i) in df.groupby('First Scan').groups))
+	
+	if params['isotopicCorrection_bool']:
+		# perform isotopic corrections and then apply them to the dataframe. No i-TRAQ copyright issues as of 2017-05-04
+		# (see logboek.txt or git history)
+		correctedIntensities, noCorrectionIndices = isotopicCorrection(getIntensities(df, intensityColumns=params['intensityColumns']),
+															  correctionsMatrix=params['isotopicCorrection_matrix'])
+		df = setIntensities(df, intensities=correctedIntensities, intensityColumns=params['intensityColumns'])
+	
 	# collapse peptide list redundancy due to multiple detections at different RT
 	# TEST here the intensity columns are alraedy lost
 	df, removedData['RT'] = collapse('RT', df, intensityColumns=params['intensityColumns'], method=params['collapse_method'],
 									 identifyingNodes=params['identifyingNodes'],
 									 undoublePSMAlgo_bool=params['undoublePSMAlgo_bool'], columnsToSave=params['collapseColumnsToSave'])
+	
 	if params['collapseCharge_bool']:
 		# collapse peptide list redundancy due to different charges (optional)
 		df, removedData['charge'] = collapse('Charge', df, intensityColumns=params['intensityColumns'], method=params['collapse_method'],
 											 identifyingNodes=params['identifyingNodes'],
 											 undoublePSMAlgo_bool=params['undoublePSMAlgo_bool'], columnsToSave=params['collapseColumnsToSave'])
+	
 	if params['collapsePTM_bool']:
 		# collapse peptide list redundancy due to different charges (optional)
 		df, removedData['modifications'] = collapse('PTM', df, intensityColumns=params['intensityColumns'], method=params['collapse_method'],
@@ -68,24 +81,16 @@ def processDf(df, params, writeToDisk, doConstand=True):
 		assert np.prod((len(i) < 2 for (s, i) in df.groupby(
 			'Annotated Sequence').groups))  # only 1 index vector in dict of SEQUENCE:[INDICES] for all sequences
 
-	if params['isotopicCorrection_bool']:
-		# perform isotopic corrections but do NOT apply them to df because this information is sensitive (copyright i-TRAQ)
-		# You can work around this by setting doConstand = False
-		intensities, noCorrectionIndices = isotopicCorrection(getIntensities(df, intensityColumns=params['intensityColumns']),
-															  correctionsMatrix=params['isotopicCorrection_matrix'])
-	else:
-		intensities = getIntensities(df, intensityColumns=params['intensityColumns'])
-
 	if doConstand:
 		# perform the CONSTANd algorithm;
+		intensities = getIntensities(df, intensityColumns=params['intensityColumns'])
 		normalizedIntensities, convergenceTrail, R, S = constand(intensities, params['accuracy'], params['maxIterations'])
 		normalizedDf = setIntensities(df, intensities=normalizedIntensities, intensityColumns=params['intensityColumns'])
 	else:
 		# TEST do NOT perform CONSTANd
 		logging.warning("+++++++++++++++++++++++++++++++CONSTAND NOT PERFORMED+++++++++++++++++++++++++++++++++")
 		normalizedIntensities = [0]
-		# WORKAROUND: apply isotopically corrected intensities to df
-		normalizedDf = setIntensities(df, intensities=intensities, intensityColumns=params['intensityColumns'])
+		normalizedDf = df
 
 	""" save results """
 	if writeToDisk:
