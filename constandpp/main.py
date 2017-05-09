c123a06d03a0f0df37eaf7f0d6d49722920a5be1
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-05-09 09:15:20 +0200

    stuff

--- constandpp/main.py
+++ constandpp/main.py
@@ -142,8 +142,8 @@ if __name__ == '__main__':  # this should not execute if main.py is not the main
 		testing = (args[6] == 'True')
 	# so if you start main.py from within web.py or something, this won't be executed
 	else:
-		doProcessing = True
-		doAnalysis = True
+		doProcessing = False
+		doAnalysis = False
 		doReport = True
 		writeToDisk = True
 		testing = False
@@ -167,7 +167,7 @@ if __name__ == '__main__':  # this should not execute if main.py is not the main
 		# jobConfigFilePath = '/home/pdiracdelta/Documents/UHasselt/CONSTANd++/jobs/2017-04-14 17:46:37.527494_alleswerkt?/jobConfig_alleswerkt?.ini'
 		# jobConfigFilePath = '/home/pdiracdelta/Documents/UHasselt/CONSTANd++/jobs/2017-04-14 18:11:41.620222_geenvraagteken/jobConfig_geenvraagteken.ini'
 		# jobConfigFilePath = '/home/pdiracdelta/Documents/UHasselt/CONSTANd++/jobs/2017-04-14 10:14:53.002433_coon2test/jobConfig_coon2test.ini'
-		jobConfigFilePath = '/home/pdiracdelta/Documents/UHasselt/CONSTANd++/jobs/2017-05-05 16:14:40.038611_testIDT_COON/jobConfig_testIDT_COON.ini'
+		jobConfigFilePath = '/home/pdiracdelta/Documents/UHasselt/CONSTANd++/jobs/2017-05-05 16:55:09.046951_testIDT_COON/jobConfig_testIDT_COON.ini'
 	
 	with app.app_context():
 		jobDirName = os.path.basename(os.path.abspath(os.path.join(jobConfigFilePath, os.pardir)))
