e9f9f8c8fa654438fd7217293dd8e2167968893c
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2017-07-13 11:41:46 +0200

    MULTIPLECONDITIONS: bugfix in generateReport() in reportFlow.py

--- constandpp/reportFlow.py
+++ constandpp/reportFlow.py
@@ -148,7 +148,7 @@ def generateReport(analysisResults, params, logFilePath, writeToDisk, processing
 		from constandpp_web.web import send_mail
 		### SEND JOB COMPLETED MAIL ###
 		mailSuccess = send_mail(recipient=params['mailRecipient'], mailBodyFile='reportMail',
-				  jobName=params['jobName'], jobID=params['jobID'], attachments=[pdfFullPath]+resultsZipFullPath)
+				  jobName=params['jobName'], jobID=params['jobID'], attachments=[pdfFullPath]+[resultsZipFullPath])
 		if mailSuccess is not None:  # something went wrong
 			import logging
 			logging.error(mailSuccess)
