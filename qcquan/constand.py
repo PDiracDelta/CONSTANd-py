783e7151e63eadd73cc72b2f52ba0037bd1f5503
Joris Van Houtven <joris.vanhoutven@uhasselt.be>
2019-10-31 10:23:00 +0100

    updated arg names in docu of constand.py

--- qcquan/constand.py
+++ qcquan/constand.py
@@ -6,8 +6,8 @@ Normalizes the data matrix <data> by raking the m by n matrix such that
 the row mean and column mean equals to 1/n. Missing information needs to
 be presented as nan values and not as zero values because CONSTANd
 employs the Matlab/NumPy functionality 'nanmean' that is able to ignore
-nan-values when calculating the mean. The variable <nbIterations> is an
-integer value that denotes the number of raking cycles. The variable <h>
+nan-values when calculating the mean. The variable <maxIterations> is an
+integer value that denotes the number of raking cycles. The variable <precision>
 defines the stopping criteria based on the L1-norm as defined by
 Friedrich Pukelsheim, Bruno Simeone in "On the Iterative Proportional
 Fitting Procedure: Structure of Accumulation Points and L1-Error
