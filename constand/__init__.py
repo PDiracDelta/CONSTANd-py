16c75c725ee657d9eb74b1b0db188f5d35741acd
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2020-07-05 13:24:56 +0200

    add __init__.py

---
+++ constand/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python implementation the CONSTANd RAS algorithm.
+"""
+
+__author__ = "Joris Van Houtven"
+__copyright__ = "Copyright ?YEAR, VITO"
+__credits__ = ["Joris Van Houtven", "Dirk Valkenborg"]
+# __license__ = "GPL"
+__version__ = "1.1"  # major.minor.patch
+__maintainer__ = "Joris Van Houtven"
+__email__ = "joris.vanhoutven@uhasselt.be" #"vanhoutvenjoris@gmail.com"
+__status__ = "Production"
+
+## GLOBAL VARIABLES ##
